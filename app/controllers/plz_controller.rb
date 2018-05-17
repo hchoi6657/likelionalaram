235ecb82596c0bcdd4fba349742c03f1f6991223
DESKTOP-7DVLGQ9\my <dlehgo14@likelion.org>
2018-05-17 12:03:06 +0900

    이미 등록된 이메일 일 때, 회원가입 안됨 --> index의 notice 부분에 "이미 등록된 사용자입니다" 표시 등록 안된 이메일로 로그인 안됨 --> index의 notice 부분에 "등록되지 않은 사용자입니다." 표시 등록된 이메일과 다른 이름으로 로그인 하려 할 때 --> notice에 "이름이 올바르지 않습니다." 표시 정상적으로 회원가입 되었을 때 --> notice에 "회원가입이 완료되었습니다." 표시
    로그인 화면 <input> 태그에서 <p> 태그로 변경하는 등 소소한 수정
    
    User 모델 비움
    rails c 에서
    User.all.each do |x| x.delete end 로 지울 수 있음

--- app/controllers/plz_controller.rb
+++ app/controllers/plz_controller.rb
@@ -1,31 +1,41 @@
 class PlzController < ApplicationController
-   def index
-     @b = Test1.new
-     @b.name = 'aa'
-     @b.save
-   end
+  def index
+    @b = Test1.new
+    @b.name = 'aa'
+    @b.save
+  end
    
-   def create
-	 if params[:sign]=="true"
-		 @user = User.new
-		 @user.name = params[:name]
-		 @user.email = params[:email]
-		 @user.save
-		 redirect_to :root
-	 else
-		 @user = User.find_by(email: params[:email])
-		 if @user
-		   redirect_to "/plz/login/#{@user.email}"
-		 else
-		   redirect_to :root
-		 end
-	 end
+  def create
+	if params[:sign]=="true"
+		
+		if User.find_by(email: params[:email])
+			redirect_to :root, notice: "이미 등록된 사용자입니다."
+		else
+			@user = User.new
+			@user.name = params[:name]
+			@user.email = params[:email]
+			@user.save
+			redirect_to :root, notice: "성공적으로 가입되었습니다."
+		end
+		
+	else
+		@user = User.find_by(email: params[:email])
+		if @user
+			if(@user.name==params[:name])
+				redirect_to "/plz/login/#{@user.email}"
+			else
+				redirect_to :root, notice: "이름이 올바르지 않습니다."
+			end
+		else
+		   redirect_to :root, notice: "등록되지 않은 사용자입니다."
+		end
+	end
 	 
-   end
+  end
    
-   def login
-     @user = User.find_by(email: params[:email]+".com")
-   end
+    def login
+      @user = User.find_by(email: params[:email]+"."+params[:format])
+    end
    
 
 end
